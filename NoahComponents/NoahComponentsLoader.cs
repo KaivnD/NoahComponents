24d4b2ccb45a1b47c83fab544dd5980de48a56ff
KaivnD <KaivnD@hotmail.com>
2019-07-03 16:00:24 +0800

    feat:增加loader

---
+++ NoahComponents/NoahComponentsLoader.cs
@@ -0,0 +1,23 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Runtime;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Noah
+{
+    public class NoahComponentsLoader : GH_AssemblyPriority
+    {
+        public NoahComponentsLoader ()
+        {
+        }
+
+        public override GH_LoadingInstruction PriorityLoad()
+        {
+            return GH_LoadingInstruction.Proceed;
+        }
+    }
+}
